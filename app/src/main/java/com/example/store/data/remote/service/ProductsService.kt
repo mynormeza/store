3a659c5a3851bfded7d3e67e987a4f786a5b0942
Mynor Meza <mynormeza92@gmail.com>
2022-05-28 09:15:21 -0600

    Fix typo on products service

--- app/src/main/java/com/example/store/data/remote/service/ProductsService.kt
+++ app/src/main/java/com/example/store/data/remote/service/ProductsService.kt
@@ -1,16 +1,14 @@
 package com.example.store.data.remote.service
 
 import com.example.store.data.remote.response.ProductsResponse
-import com.example.store.domain.model.Product
 import retrofit2.Call
 import retrofit2.Retrofit
 import javax.inject.Inject
 import javax.inject.Singleton
 
-
 @Singleton
 class ProductsService @Inject constructor(retrofit: Retrofit) : ProductsApi {
-    private val productsApi: ProductsApi by lazy { retrofit.create(productsApi::class.java) }
+    private val productsApi: ProductsApi by lazy { retrofit.create(ProductsApi::class.java) }
 
     override fun getProducts(): Call<ProductsResponse> {
         return productsApi.getProducts()
